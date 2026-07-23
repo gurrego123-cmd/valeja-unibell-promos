90bc443daaff945930a0d75c1f69f95a920ddcb0
gurrego123-cmd <gurrego123@gmail.com>
2026-07-23 06:09:04 +0000

    Corregir configuración de GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
-  plugins: [react()],
-})
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
   plugins: [react()],
     base: '/valeja-unibell-promos/',
     })
